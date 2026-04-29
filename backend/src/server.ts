c8b3e3ae44c0562b7c95c4fbe81336010aa96946
iammadsfq <13523135@std.stei.itb.ac.id>
2026-04-29 09:39:08 +0700

    chore: setup supabase

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,25 @@
-import app from './app'
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import { supabase } from './config/supabase'
 
-const PORT = 5000
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/health', (req: Request, res: Response) => {
+  res.json({
+    status: 'success',
+    message: 'Server is running'
+  })
+})
+
+app.get('/test-supabase', async (req: Request, res: Response) => {
+  const { data, error } = await supabase.from('hackathon_test').select('*');
+  if(error) return res.status(500).json(error);
+  return res.json(data);
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
