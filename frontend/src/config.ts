2fd10855a2cf199d7c29f126d820a171c6eea77d
iammadsfq <13523135@std.stei.itb.ac.id>
2026-05-05 17:50:38 +0700

    chore: update config api url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 // Jika berjalan di Vercel (production), gunakan "/api"
 // Jika berjalan di laptop (development), gunakan "http://localhost:5000"
-export const API_BASE_URL = process.env.NODE_ENV === 'production' 
+export const API_URL = process.env.NODE_ENV === 'production' 
   ? '/api' 
   : 'http://localhost:5000';
